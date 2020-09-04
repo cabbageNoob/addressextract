6ffe4d0b319aa81245c7913f312b69e3ee808adb
cjh <492795090@qq.com>
2020-09-04 20:44:25 +0800

    地址解析，自动补全省、市

---
+++ addressextract/exceptions.py
@@ -0,0 +1,15 @@
+'''
+Descripttion: 
+Author: cjh (492795090@qq.com)
+Date: 2020-09-04 20:07:10
+'''
+class CPCAException(Exception):
+    pass
+
+
+class PlaceTypeNotExistException(CPCAException):
+    pass
+
+
+class InputTypeNotSuportException(CPCAException):
+    pass
